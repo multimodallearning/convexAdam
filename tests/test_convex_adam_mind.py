d812c89e339203f58e110cb51e8c907fd7d392a0
Joeran Bosma <joeran@bosma.co>
2024-02-19 16:15:33 +0100

    Test for rotation and translation
    Towards applying the transformation on the anisotropic image

--- tests/test_convex_adam_mind.py
+++ tests/test_convex_adam_mind.py
@@ -252,9 +252,78 @@ def test_convex_adam_identity_rotated_direction(
     assert np.allclose(disp_center, np.zeros_like(disp_center), atol=0.3)
 
 
+def test_convex_adam_identity_rotated_and_shifted(
+    input_dir = Path("tests/input"),
+    output_dir = Path("tests/output"),
+    subject_id = "10000_1000000",
+):
+    # paths
+    patient_id = subject_id.split("_")[0]
+    fixed_image = sitk.ReadImage(str(input_dir / patient_id / f"{subject_id}_t2w.mha"))
+    moving_image = sitk.ReadImage(str(input_dir / patient_id / f"{subject_id}_t2w.mha"))
+
+    # set center and direction to unity
+    moving_image.SetDirection([1, 0, 0, 0, 1, 0, 0, 0, 1])
+    moving_image.SetOrigin([0, 0, 0])
+    fixed_image.SetDirection([1, 0, 0, 0, 1, 0, 0, 0, 1])
+    fixed_image.SetOrigin([0, 0, 0])
+    sitk.WriteImage(fixed_image, str(output_dir / patient_id / f"{subject_id}_fixed_unity.mha"))
+
+    # rotate the moving image twice: once by updating the direction cosines and once by resampling the image
+    angle = np.pi / 4.0
+    moving_image = rotate_image_around_center_resample(moving_image, angle)
+    rotate_image_around_center_affine(moving_image, angle)
+
+    # translate the moving image
+    affine = sitk.AffineTransform(3)
+    affine.SetTranslation([20, 0, 0])
+    moving_image = sitk.Resample(moving_image, affine)
+    sitk.WriteImage(moving_image, str(output_dir / patient_id / f"{subject_id}_moving_rotated_and_shifted.mha"))
+    # note: the moving image, when viewed in ITK-SNAP, is now moved 20 mm to the left (patient's right)
+
+    # resample images to specified spacing and the field of view of the fixed image
+    fixed_image_resampled = resample_img(fixed_image, spacing=(1.0, 1.0, 1.0))
+    moving_image_resampled = resample_moving_to_fixed(fixed_image_resampled, moving_image)
+    sitk.WriteImage(fixed_image_resampled, str(output_dir / patient_id / f"{subject_id}_fixed_resampled.mha"))
+
+    # run convex adam
+    displacementfield = convex_adam_pt(
+        img_fixed=fixed_image_resampled,
+        img_moving=moving_image_resampled,
+    )
+
+    disp = sitk.GetImageFromArray(displacementfield.astype(np.float32))
+    disp.CopyInformation(fixed_image_resampled)
+    sitk.WriteImage(disp, str(output_dir / patient_id / f"{subject_id}_displacementfield.mha"))
+
+    # apply displacement field
+    moving_image_resampled_warped = apply_convex(
+        disp=displacementfield,
+        moving=moving_image_resampled,
+    )
+
+    # convert to SimpleITK image
+    moving_image_resampled_warped = sitk.GetImageFromArray(moving_image_resampled_warped.astype(np.float32))
+    moving_image_resampled_warped.CopyInformation(moving_image_resampled)
+
+    # save warped image
+    output_dir.mkdir(exist_ok=True, parents=True)
+    sitk.WriteImage(moving_image_resampled_warped, str(output_dir / patient_id / f"{subject_id}_moving_rotated_and_shifted_warped.mha"))
+
+    # apply displacement field to the moving image without resampling
+    moving_image_warped = apply_convex(
+        disp=displacementfield,
+        moving=moving_image,
+    )
+    moving_image_warped = sitk.GetImageFromArray(moving_image_warped.astype(np.float32))
+    moving_image_warped.CopyInformation(fixed_image_resampled)
+    sitk.WriteImage(moving_image_resampled_warped, str(output_dir / patient_id / f"{subject_id}_original_moving_rotated_and_shifted_warped.mha"))
+
+
 if __name__ == "__main__":
     test_convex_adam_identity()
     test_convex_adam()
     test_convex_adam_translation()
     test_convex_adam_identity_rotated_direction()
+    test_convex_adam_identity_rotated_and_shifted()
     print("All tests passed")
